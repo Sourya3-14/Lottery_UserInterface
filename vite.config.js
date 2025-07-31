c14066bad88237eab43120127cb34f1ab1da6a49
Sourya <souryaadhikary2911@gmail.com>
2025-08-01 01:58:40 +0530

    Added base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-	plugins: [react()]
-	// base: './', // Use relative paths for IPFS
-	// build: {
-	//   outDir: 'dist',
-	//   assetsDir: 'assets',
-	//   sourcemap: false, // Optional: disable for smaller builds
-	// }
+	plugins: [react()],
+	base: process.env.VITE_BASE_PATH || "/ottery_UserInterface",
+	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+		sourcemap: false, // Optional: disable for smaller builds
+	},
 })
